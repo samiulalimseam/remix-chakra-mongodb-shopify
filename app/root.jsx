82afefb78de6ce4a4096c87d20e068bc7b3e2ea7
Al-Amin Hossain <alaminhossain.tech@gmail.com>
2023-09-09 10:35:30 +0600

    chakra-shopify-boilerplate

---
+++ app/root.jsx
@@ -0,0 +1,30 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ChakraProvider>
+          <Outlet />
+        </ChakraProvider>
+        <ScrollRestoration />
+        <LiveReload />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/app._index.jsx
@@ -0,0 +1,93 @@
+import { json } from "@remix-run/node";
+import {
+  useActionData,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
+import { useEffect } from "react";
+
+import { Box, Container, Heading } from "@chakra-ui/react";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  const { session } = await authenticate.admin(request);
+
+  return json({ shop: session.shop.replace(".myshopify.com", "") });
+};
+
+export async function action({ request }) {
+  const { admin } = await authenticate.admin(request);
+
+  const color = ["Red", "Orange", "Yellow", "Green"][
+    Math.floor(Math.random() * 4)
+  ];
+  const response = await admin.graphql(
+    `#graphql
+      mutation populateProduct($input: ProductInput!) {
+        productCreate(input: $input) {
+          product {
+            id
+            title
+            handle
+            status
+            variants(first: 10) {
+              edges {
+                node {
+                  id
+                  price
+                  barcode
+                  createdAt
+                }
+              }
+            }
+          }
+        }
+      }`,
+    {
+      variables: {
+        input: {
+          title: `${color} Snowboard`,
+          variants: [{ price: Math.random() * 100 }],
+        },
+      },
+    }
+  );
+
+  const responseJson = await response.json();
+
+  return json({
+    product: responseJson.data.productCreate.product,
+  });
+}
+
+export default function Index() {
+  const nav = useNavigation();
+  const { shop } = useLoaderData();
+  const actionData = useActionData();
+  const submit = useSubmit();
+
+  const isLoading =
+    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
+
+  const productId = actionData?.product?.id.replace(
+    "gid://shopify/Product/",
+    ""
+  );
+
+  useEffect(() => {
+    if (productId) {
+      shopify.toast.show("Product created");
+    }
+  }, [productId]);
+
+  const generateProduct = () => submit({}, { replace: true, method: "POST" });
+
+  return (
+    <Container>
+      <Box>
+        <Heading>Hello World</Heading>
+      </Box>
+    </Container>
+  );
+}
